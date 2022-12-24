26a92ce8942ce39f80579aeafdd16ef48f9450e4
mangaltalekar1974 <mangaltalekar1974@gmail.com>
2022-12-25 00:29:28 +0530

    last commit

--- LeapYea/src/Leap.java
+++ LeapYea/src/Leap.java
@@ -1,15 +1,15 @@
 
 public class Leap {
-	public static void main (String args[]) {
-		int year=1997;
-		if (year%4==0) {
+	public static void main(String args[]) {
+		int year = 1997;
+		if (year % 4 == 0) {
 			System.out.println("year is leap");
-		}
-		else {
+		} else {
 			System.out.println("year is not leap");
-			
+
 		}
-				
+		System.out.println(" ");
+
 	}
 
 }
